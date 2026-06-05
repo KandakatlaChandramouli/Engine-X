02236316df8c45797fd498fdb74a11c56c74afe5
Chandra Mouli Kandakatla <167426948+KandakatlaChandramouli@users.noreply.github.com>
2026-06-05 16:10:49 +0000

    phase20: root search validation

--- core/btree_insert_recursive_test.go
+++ core/btree_insert_recursive_test.go
@@ -1,1 +1,69 @@
 package core
+
+import "testing"
+
+func TestRootBecomesSearchableAfterPromotion(
+	t *testing.T,
+) {
+
+	pager := NewPager()
+
+	var root Page
+
+	InitPage(
+		&root,
+		1,
+	)
+
+	pager.Add(
+		&root,
+	)
+
+	inserted := 0
+
+	for {
+
+		result,
+			ok :=
+			InsertRecursive(
+				pager,
+				1,
+				[]byte(string(rune('a'+(inserted%26)))),
+				[]byte("value"),
+			)
+
+		if !ok {
+			t.Fatal()
+		}
+
+		if result.Split {
+
+			if !HandleRootSplit(
+				&root,
+				result,
+			) {
+				t.Fatal()
+			}
+
+			page,
+				ok :=
+				BTreeSearch(
+					pager,
+					1,
+					[]byte("z"),
+				)
+
+			if !ok {
+				t.Fatal()
+			}
+
+			if page == nil {
+				t.Fatal()
+			}
+
+			return
+		}
+
+		inserted++
+	}
+}

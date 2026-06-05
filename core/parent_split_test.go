733df480d5b2f90c8bb77e60071ebc5ff207a325
Chandra Mouli Kandakatla <167426948+KandakatlaChandramouli@users.noreply.github.com>
2026-06-05 16:09:52 +0000

    phase19: runtime parent split execution

---
+++ core/parent_split_test.go
@@ -0,0 +1,55 @@
+package core
+
+import "testing"
+
+func TestInternalSplitProducesSeparator(
+	t *testing.T,
+) {
+
+	var parent Page
+	var sibling Page
+
+	InitInternalPage(
+		&parent,
+		1,
+	)
+
+	InitInternalPage(
+		&sibling,
+		2,
+	)
+
+	SetLeftChild(
+		&parent,
+		100,
+	)
+
+	for i := 0; i < 200; i++ {
+
+		ok :=
+			InternalInsert(
+				&parent,
+				[]byte(string(rune('a'+(i%26)))),
+				uint64(i+1000),
+			)
+
+		if !ok {
+			break
+		}
+	}
+
+	result,
+		ok :=
+		InternalSplit(
+			&parent,
+			&sibling,
+		)
+
+	if !ok {
+		t.Fatal()
+	}
+
+	if len(result.SeparatorKey) == 0 {
+		t.Fatal()
+	}
+}

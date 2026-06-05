603f3a47480eef66c6e02fb76c49f270594fe8b3
Chandra Mouli Kandakatla <167426948+KandakatlaChandramouli@users.noreply.github.com>
2026-06-05 03:32:58 +0000

    phase5: slot accessor abstraction

---
+++ core/slot.go
@@ -0,0 +1,66 @@
+package core
+
+import "encoding/binary"
+
+type Slot struct {
+	Offset uint16
+	KeyLen uint16
+	ValLen uint16
+	Flags  uint16
+}
+
+func ReadSlot(
+	p *Page,
+	index uint16,
+) Slot {
+
+	slotPos :=
+		HeaderSize +
+			int(index)*SlotSize
+
+	return Slot{
+		Offset: binary.LittleEndian.Uint16(
+			p.Data[slotPos : slotPos+2],
+		),
+		KeyLen: binary.LittleEndian.Uint16(
+			p.Data[slotPos+2 : slotPos+4],
+		),
+		ValLen: binary.LittleEndian.Uint16(
+			p.Data[slotPos+4 : slotPos+6],
+		),
+		Flags: binary.LittleEndian.Uint16(
+			p.Data[slotPos+6 : slotPos+8],
+		),
+	}
+}
+
+func WriteSlot(
+	p *Page,
+	index uint16,
+	s Slot,
+) {
+
+	slotPos :=
+		HeaderSize +
+			int(index)*SlotSize
+
+	binary.LittleEndian.PutUint16(
+		p.Data[slotPos:slotPos+2],
+		s.Offset,
+	)
+
+	binary.LittleEndian.PutUint16(
+		p.Data[slotPos+2:slotPos+4],
+		s.KeyLen,
+	)
+
+	binary.LittleEndian.PutUint16(
+		p.Data[slotPos+4:slotPos+6],
+		s.ValLen,
+	)
+
+	binary.LittleEndian.PutUint16(
+		p.Data[slotPos+6:slotPos+8],
+		s.Flags,
+	)
+}

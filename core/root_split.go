da2420220ac3da6f65bc6df3826a68021873de35
Chandra Mouli Kandakatla <167426948+KandakatlaChandramouli@users.noreply.github.com>
2026-06-05 16:07:35 +0000

    phase17: root promotion after leaf split

--- core/root_split.go
+++ core/root_split.go
@@ -1,1 +1,14 @@
 package core
+
+func HandleRootSplit(
+	root *Page,
+	split InsertRecursiveResult,
+) bool {
+
+	return PromoteRoot(
+		root,
+		PageID(root),
+		split.RightPageID,
+		split.SeparatorKey,
+	)
+}
--- core/root_split_test.go
+++ core/root_split_test.go
@@ -1,1 +1,62 @@
 package core
+
+import "testing"
+
+func TestPromoteRootAfterLeafSplit(
+	t *testing.T,
+) {
+
+	pager := NewPager()
+
+	var root Page
+
+	InitPage(
+		&root,
+		1,
+	)
+
+	pager.Add(
+		&root,
+	)
+
+	inserted := 0
+
+	for {
+
+		result,
+			ok :=
+			InsertRecursive(
+				pager,
+				1,
+				[]byte(string(rune('a'+(inserted%26)))),
+				[]byte("value"),
+			)
+
+		if !ok {
+			t.Fatal()
+		}
+
+		if result.Split {
+
+			ok =
+				PromoteRoot(
+					&root,
+					1,
+					result.RightPageID,
+					result.SeparatorKey,
+				)
+
+			if !ok {
+				t.Fatal()
+			}
+
+			if PageType(&root) != PageTypeInternal {
+				t.Fatal()
+			}
+
+			return
+		}
+
+		inserted++
+	}
+}

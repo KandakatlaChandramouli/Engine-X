7583e05468a7ec025eb334de7edb5c32268b501a
Chandra Mouli Kandakatla <167426948+KandakatlaChandramouli@users.noreply.github.com>
2026-06-05 16:37:01 +0000

    phase42: internal split balance validation

---
+++ core/internal_split_balance_test.go
@@ -0,0 +1,74 @@
+package core
+
+import "testing"
+
+func TestInternalSplitBalance(
+	t *testing.T,
+) {
+
+	var left Page
+	var right Page
+
+	InitInternalPage(
+		&left,
+		1,
+	)
+
+	InitInternalPage(
+		&right,
+		2,
+	)
+
+	SetLeftChild(
+		&left,
+		100,
+	)
+
+	for i := 0; i < 100; i++ {
+
+		if !InternalInsert(
+			&left,
+			[]byte{
+				byte(i),
+			},
+			uint64(i+1000),
+		) {
+			break
+		}
+	}
+
+	result,
+		ok :=
+		InternalSplit(
+			&left,
+			&right,
+		)
+
+	if !ok {
+		t.Fatal()
+	}
+
+	if len(
+		result.SeparatorKey,
+	) == 0 {
+		t.Fatal()
+	}
+
+	leftCount :=
+		InternalEntryCount(
+			&left,
+		)
+
+	rightCount :=
+		InternalEntryCount(
+			&right,
+		)
+
+	if leftCount == 0 {
+		t.Fatal()
+	}
+
+	if rightCount == 0 {
+		t.Fatal()
+	}
+}

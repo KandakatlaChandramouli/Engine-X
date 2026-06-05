73ea0dec8446be87d456326e36c1f9161cdcf1ac
Chandra Mouli Kandakatla <167426948+KandakatlaChandramouli@users.noreply.github.com>
2026-06-05 16:16:53 +0000

    phase23: parent propagation foundation

---
+++ core/deep_tree_growth_test.go
@@ -0,0 +1,41 @@
+package core
+
+import (
+        "fmt"
+        "testing"
+)
+
+func TestDeepTreeGrowth(
+        t *testing.T,
+) {
+
+        pager := NewPager()
+
+        var root Page
+
+        InitPage(
+                &root,
+                1,
+        )
+
+        pager.Add(
+                &root,
+        )
+
+        for i := 0; i < 5000; i++ {
+
+                key :=
+                        []byte(
+                                fmt.Sprintf(
+                                        "key-%05d",
+                                        i,
+                                ),
+                        )
+
+                _,
+                        key
+
+                _,
+                        pager
+        }
+}
--- core/parent_propagation_test.go
+++ core/parent_propagation_test.go
@@ -2,23 +2,83 @@ package core
 
 import "testing"
 
-func TestParentAwarePropagation(
+func TestLeafSplitCanBeInsertedIntoParent(
 	t *testing.T,
 ) {
 
-	var child Page
+	pager := NewPager()
+
+	var parent Page
+	var leaf Page
+	var sibling Page
+
+	InitInternalPage(
+		&parent,
+		1,
+	)
 
 	InitPage(
-		&child,
-		10,
+		&leaf,
+		2,
 	)
 
-	SetParentPageID(
-		&child,
-		99,
+	InitPage(
+		&sibling,
+		3,
 	)
 
-	if ParentPageID(&child) != 99 {
+	SetLeftChild(
+		&parent,
+		2,
+	)
+
+	pager.Add(&parent)
+	pager.Add(&leaf)
+	pager.Add(&sibling)
+
+	inserted := 0
+
+	for {
+
+		ok :=
+			LeafInsert(
+				&leaf,
+				[]byte(string(rune('a'+(inserted%26)))),
+				[]byte("value"),
+			)
+
+		if !ok {
+			break
+		}
+
+		inserted++
+	}
+
+	split,
+		ok :=
+		LeafSplit(
+			&leaf,
+			&sibling,
+		)
+
+	if !ok {
+		t.Fatal()
+	}
+
+	ok =
+		InsertIntoParent(
+			&parent,
+			split.SeparatorKey,
+			PageID(&sibling),
+		)
+
+	if !ok {
+		t.Fatal()
+	}
+
+	if InternalEntryCount(
+		&parent,
+	) == 0 {
 		t.Fatal()
 	}
 }

e071a97df0b2ede0c67093bd7f40f19207453fba
Chandra Mouli Kandakatla <167426948+KandakatlaChandramouli@users.noreply.github.com>
2026-06-06 08:12:53 +0000

    phase70 analysis pass foundation

--- internal/aries/analysis.go
+++ internal/aries/analysis.go
@@ -1,10 +1,46 @@
 package aries
 
+type TransactionStatus uint8
+
+const (
+        Active TransactionStatus = iota
+        Committed
+        Aborted
+)
+
 type TransactionEntry struct {
-        TxID uint64
+        TxID   uint64
+        Status TransactionStatus
+        LastLSN uint64
 }
 
 type DirtyPageEntry struct {
         PageID uint64
         RecLSN uint64
+}
+
+type AnalysisResult struct {
+        Transactions map[uint64]TransactionEntry
+        DirtyPages   map[uint64]DirtyPageEntry
+}
+
+func Analyze(
+        txns []TransactionEntry,
+        pages []DirtyPageEntry,
+) AnalysisResult {
+
+        r := AnalysisResult{
+                Transactions: make(map[uint64]TransactionEntry),
+                DirtyPages:   make(map[uint64]DirtyPageEntry),
+        }
+
+        for _, tx := range txns {
+                r.Transactions[tx.TxID] = tx
+        }
+
+        for _, p := range pages {
+                r.DirtyPages[p.PageID] = p
+        }
+
+        return r
 }
--- internal/aries/analysis_test.go
+++ internal/aries/analysis_test.go
@@ -2,7 +2,29 @@ package aries
 
 import "testing"
 
-func TestCompile(
+func TestAnalyze(
         t *testing.T,
 ) {
+        r := Analyze(
+                []TransactionEntry{
+                        {
+                                TxID: 1,
+                                Status: Active,
+                        },
+                },
+                []DirtyPageEntry{
+                        {
+                                PageID: 10,
+                                RecLSN: 100,
+                        },
+                },
+        )
+
+        if len(r.Transactions) != 1 {
+                t.Fatal()
+        }
+
+        if len(r.DirtyPages) != 1 {
+                t.Fatal()
+        }
 }
